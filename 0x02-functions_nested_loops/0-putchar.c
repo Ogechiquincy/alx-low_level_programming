a21b62bd16dd2ceeb87d46245d386ba9db386beb
Ogechiquincy <goldquincy1242@gmail.com>
2022-04-13 07:39:51 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
-@@ -0,0 +1,23 @@
-#include "main.h"
+
+include "main.h"
 
 /**
  * main - prints _putchar
@@ -11,14 +11,14 @@
 
 int main(void)
 {
-	char text[9] = "_putchar";
-	int i = 0;
+        char text[9] = "_putchar";
+        int i = 0;
 
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(text[i]);
-	}
-	_putchar('\n');
+        for (i = 0; i < 8; i++)
+        {
+                _putchar(text[i]);
+        }
+        _putchar('\n');
 
-	return (0);
+        return (0);
 }
