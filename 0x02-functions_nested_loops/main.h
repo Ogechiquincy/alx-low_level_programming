88ff0dab0b1ed8a9349233b3aa6a00fd3f040a43
Ogechiquincy <goldquincy1242@gmail.com>
2022-04-13 03:57:24 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -10,18 +9,13 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
+int _abs(int n);
+int print_last_digit(int n);
 void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-=======
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
->>>>>>> 0a388c30908ad8119f6ca64a48c10d824f9c0506
 void print_times_table(int n);
-
-#endif
