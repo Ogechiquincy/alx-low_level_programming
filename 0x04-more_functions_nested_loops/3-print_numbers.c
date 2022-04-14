d1fd16cd3e712688dbc0170753298e944ff383a7
Ogechiquincy <goldquincy1242@gmail.com>
2022-04-14 06:02:04 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,27 @@
-#​include​ ​"​main.h​" 
+#include "main.h"
 
- /**
- * print_numbers - prints numbers 0-9 
- ​ * 
- ​ * Return: void 
- ​ ​*/ 
-  
- ​void​ ​print_numbers​(​void​) 
- ​{ 
- ​        ​int​ c; 
-  
- ​        ​for​ (c = ​'​0​'​; c <= ​'​9​'​; c++) 
- ​        { 
- ​                ​_putchar​(c); 
- ​        } 
- ​        ​_putchar​(​'​\n​'​); 
- ​}
+/**
+ * print_most_numbers - a function that prints the numbers, from 0 to 9.
+ * Do not print 2 and 4.
+ * followed by a new line.
+ * Return: Always 0
+ * print_numbers - prints numbers 0-9
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+
+void print_numbers(void)
+{
+	int i = '0';
+	int c;
+
+	for (; i <= '9'; i++)
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (i != '2' && i != '4')
+			_putchar(i);
+		_putchar(c);
+	}
+	_putchar('\n');
+}
