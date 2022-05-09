dc880d87df022d17d538ef6e605e7c83bbd8ad64
Ogechiquincy <goldquincy1242@gmail.com>
2022-05-09 05:32:03 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,16 +2,15 @@
 #include <stdlib.h>
 
 /**
- * _atoi_digit - convert a char to integer.
+* _atoi_digit - convert a char to integer.
  * @x: character to convert.
  * Return: integer.
  **/
 
 int _atoi_digit(char x)
 {
-	unsigned int res;
-
-	if (x <= '9' && x >= '0')
+unsigned int res;
+if (x <= '9' && x >= '0')
 		res = x - '0';
 	return (res);
 }
@@ -25,14 +24,14 @@ int _isNumber(char *argv)
 {
 	int i;
 
-	for (i = 0; argv[i]; i++)
+for (i = 0; argv[i]; i++)
 		if (argv[i] < 48 || argv[i] > 57)
 			return (1);
 	return (0);
 }
 
 /**
- *_calloc - allocate array of size * nmemb.
+*_calloc - allocate array of size * nmemb.
  * @nmemb: number of elements.
  * @size: size of element.
  * Return: pointer to array.
@@ -47,8 +46,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (tab == NULL)
 		return (NULL);
-
-	for (i = 0; i < (size * nmemb); i++)
+for (i = 0; i < (size * nmemb); i++)
 		tab[i] = '0';
 
 	return (tab);
@@ -66,9 +64,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 void *mul_array(char *a1, int len1, char a2, char *a3, int lena)
 {
-	int mul = 0, i, k;
-
-	k = lena;
+int mul = 0, i, k;
+k = lena;
 	for (i = len1 - 1; i >= 0 ; i--)
 	{
 		mul += (a1[i] - '0') * (a2 - '0') + (a3[k] - '0');
@@ -79,12 +76,11 @@ void *mul_array(char *a1, int len1, char a2, char *a3, int lena)
 
 		while (mul != 0)
 		{
-			mul += a3[k] - '0';
+mul += a3[k] - '0';
 			a3[k] = (mul % 10) + '0';
 			mul /= 10;
 			k--;
 		}
-
 	return (a3);
 }
 /**
@@ -116,19 +112,18 @@ void print_array(char *a, int nb)
 
 int main(int argc, char *argv[])
 {
-	int i, c, len1, len2, lenres;
+int i, c, len1, len2, lenres;
 	char E[6] = {'E', 'r', 'r', 'o', 'r', '\n'};
 	char *tabres;
 
 	if (argc != 3 || _isNumber(argv[1]) == 1 || _isNumber(argv[2]) == 1)
-	{
-		for (i = 0; i < 6; i++)
+	{for (i = 0; i < 6; i++)
 		{
-			_putchar(E[i]);
+_putchar(E[i]);
 		}
 		exit(98);
 	}
-	for (len1 = 0; argv[1][len1]; len1++)
+for (len1 = 0; argv[1][len1]; len1++)
 	;
 	for (len2 = 0; argv[2][len2]; len2++)
 	;
